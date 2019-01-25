f737eed4d8dbf11d6d9fb779a735ed3752bc7f5d
kingmax <kingmax_res@163.com>
2019-01-25 17:14:12 +0800

    + test dynamic import other lib

--- startupScript.py
+++ startupScript.py
@@ -3,3 +3,15 @@ def runStartupScript():
 
 if __name__ == '__main__':
     runStartupScript()
+
+    # test using other lib
+    import sys
+    import os
+    myLibPath = os.path.join(os.path.dirname(__file__), 'test_lib')
+    print(myLibPath)
+    sys.path.append(myLibPath)
+    import _myLibs
+    print('dynamic import other function lib succuss!')
+    _myLibs.sayHi('Jason.Li')
+    print(_myLibs.add(100, 100))
+    print('startup script done.')
---
+++ test_lib/_myLibs.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+# testing for startupScript dynamic import this lib
+
+def sayHi(n=''):
+    print('Hi, %s'%n)
+
+def add(a, b):
+    return a+b;
+
+if __name__ == '__main__':
+    print('run myLibs.py')
