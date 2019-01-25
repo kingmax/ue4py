4004e05afb44bbef819cfeddd89aebb957d2863b
kingmax <kingmax_res@163.com>
2019-01-25 19:35:55 +0800

    starting...

---
+++ assetFunctions.py
@@ -0,0 +1,6 @@
+import unreal
+
+# ref: https://www.youtube.com/watch?v=M9UlyVtD5Dk&index=3&list=PLBLmKCAjA25Br8cOVzUroqi_Nwipg-IdP
+
+def buildImportTask(filename, destination_path):
+    task = unreal.AssetImportTask()
