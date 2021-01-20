06bb44260b052f92ef16da64a98515560a421229
kingmax <kingmax_res@163.com>
2021-01-20 13:40:33 +0800

    + UnrealEnginePython plugin for ue4.18.3

--- ue420/hello.py
+++ ue420/hello.py
@@ -2,3 +2,7 @@ import unreal as ue4
 import sys
 
 ue4.log('hello, unreal. this message from Python {}'.format(sys.version))
+
+# https://github.com/20tab/UnrealEnginePython
+# https://github.com/20tab/UnrealEnginePython/blob/master/tutorials/YourFirstAutomatedPipeline.md
+# https://github.com/20tab/UnrealEnginePython/tree/master/examples
